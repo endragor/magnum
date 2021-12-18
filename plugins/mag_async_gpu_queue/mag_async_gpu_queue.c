8e3d6401561068eb7d88d0e03b02289157fc1837
Ruslan Mustakov <ruslan.mustakov@xored.com>
2021-12-18 16:52:18 +0700

    Implement async GPU queue

---
+++ plugins/mag_async_gpu_queue/mag_async_gpu_queue.c
@@ -0,0 +1,501 @@
+static struct tm_temp_allocator_api *tm_temp_allocator_api;
+static struct tm_allocator_api *tm_allocator_api;
+static struct tm_os_api *tm_os_api;
+
+#include <foundation/allocator.h>
+#include <foundation/api_registry.h>
+#include <foundation/atomics.inl>
+#include <foundation/carray.inl>
+#include <foundation/os.h>
+
+#include <plugins/renderer/render_backend.h>
+
+#include "plugins/mag_async_gpu_queue/mag_async_gpu_queue.h"
+
+#define THREAD_STACK_SIZE (256 * 1024)
+
+typedef struct task_t
+{
+    void (*f)(mag_async_gpu_queue_task_args_t *args);
+    void *data;
+    tm_allocator_i *data_allocator;
+    uint64_t data_size;
+    uint64_t priority;
+} task_t;
+
+typedef struct executing_task_t
+{
+    uint64_t task_id;
+    mag_async_gpu_queue_fence_t *fences;
+} executing_task_t;
+
+typedef struct mag_async_gpu_queue_o
+{
+    tm_allocator_i allocator;
+    tm_renderer_backend_i *backend;
+    uint32_t max_simultaneous_tasks;
+
+    tm_thread_o thread;
+    atomic_uint_least32_t shutdown;
+
+    atomic_uint_least64_t next_id;
+
+    tm_critical_section_o tasks_cs;
+    tm_semaphore_o sem;
+    task_t *task_heap;
+    // IDs match the heap order but are stored separately to make the
+    // linear search faster by not polluting the CPU cache.
+    uint64_t *task_ids;
+
+    tm_critical_section_o executing_tasks_cs;
+    executing_task_t *executing_tasks;
+
+    tm_critical_section_o completed_tasks_cs;
+    // completed but not yet confirmed (by is_task_done()) tasks
+    uint64_t *completed_tasks;
+    // canceled but currently executing tasks
+    uint64_t *canceled_tasks;
+} mag_async_gpu_queue_o;
+
+// Returns the index of parent item of the heap item at index `i`.
+static inline uint32_t heap__parent(uint32_t i)
+{
+    return i > 0 ? (i - 1) / 2 : UINT32_MAX;
+}
+
+// Returns the index of the first child of the heap item at index `i`.
+static inline uint32_t heap__first_child(uint32_t i)
+{
+    return i * 2 + 1;
+}
+
+// Swaps the heap items at indices `a` and `b`.
+static void heap__swap(task_t *heap, uint64_t *task_ids, uint32_t a, uint32_t b)
+{
+    const struct task_t tmp = heap[a];
+    heap[a] = heap[b];
+    heap[b] = tmp;
+
+    const uint64_t tmp_id = task_ids[a];
+    task_ids[a] = task_ids[b];
+    task_ids[b] = tmp_id;
+}
+
+// Updates the heap so that the heap invariant is maintained after the heap value at index `i` has
+// changed.
+static void heap__update(struct task_t *heap, uint64_t *task_ids, uint32_t i)
+{
+    const uint32_t n = (uint32_t)tm_carray_size(heap);
+
+    // As long as item has a parent and is smaller than its parent, swap.
+    while (i) {
+        const uint32_t p = heap__parent(i);
+        if (i < n && heap[i].priority < heap[p].priority) {
+            heap__swap(heap, task_ids, i, p);
+            i = p;
+        } else
+            break;
+    }
+
+    // As long as item has a child that is smaller than the item, swap the item with its smallest
+    // child.
+    while (true) {
+        const uint32_t l = heap__first_child(i);
+        if (l >= n)
+            break;
+        const uint32_t r = l + 1;
+        const uint32_t smallest = r >= n || heap[l].priority < heap[r].priority ? l : r;
+        if (heap[smallest].priority < heap[i].priority) {
+            heap__swap(heap, task_ids, i, smallest);
+            i = smallest;
+        } else
+            break;
+    }
+}
+
+// Pops the top item from the heap and returns it.
+static task_t heap__pop(task_t *heap, uint64_t *task_ids, uint64_t *out_id)
+{
+    task_t res = heap[0];
+    *out_id = task_ids[0];
+    heap__swap(heap, task_ids, 0, (uint32_t)tm_carray_size(heap) - 1);
+    tm_carray_pop(heap);
+    tm_carray_pop(task_ids);
+    heap__update(heap, task_ids, 0);
+    return res;
+}
+
+// Pushes the item `h` to the heap.
+static void heap__push(task_t **heap, uint64_t **task_ids, task_t task, uint64_t id, tm_allocator_i *a)
+{
+    tm_carray_push(*heap, task, a);
+    tm_carray_push(*task_ids, id, a);
+    heap__update(*heap, *task_ids, (uint32_t)tm_carray_size(*heap) - 1);
+}
+
+// Deletes element at index i from the heap
+static void heap__remove(task_t *heap, uint64_t *task_ids, uint32_t i)
+{
+    uint64_t task_count = tm_carray_size(task_ids);
+    task_ids[i] = tm_carray_pop(task_ids);
+    heap[i] = tm_carray_pop(heap);
+    if (i < task_count - 1) {
+        heap__update(heap, task_ids, i);
+    }
+}
+
+static void wait_for_fences(tm_renderer_backend_i *backend, mag_async_gpu_queue_fence_t *fences, bool *signaled, uint32_t n)
+{
+    bool something_signaled = false;
+    while (!something_signaled) {
+        for (uint64_t i = 0; i < tm_carray_size(fences); ++i) {
+            if (backend->read_complete(backend->inst, fences[i].fence_id, fences[i].device_affinity_mask)) {
+                signaled[i] = true;
+                something_signaled = true;
+            }
+        }
+    }
+}
+
+// Waits until at least one of the tasks is fully completed.
+static uint64_t *wait_for_task(mag_async_gpu_queue_o *q, executing_task_t *wait_tasks, tm_allocator_i *a)
+{
+    uint64_t *completed_ids = NULL;
+    while (!tm_carray_size(completed_ids)) {
+        mag_async_gpu_queue_fence_t *fences = NULL;
+        struct fence_index
+        {
+            uint32_t task_i;
+            uint32_t fence_i;
+        } *fence_indexes = NULL;
+        for (uint32_t i = 0; i < tm_carray_size(wait_tasks); ++i) {
+            tm_carray_push_array(fences, wait_tasks[i].fences, tm_carray_size(wait_tasks[i].fences), a);
+            for (uint32_t ri = 0; ri < tm_carray_size(wait_tasks[i].fences); ++ri) {
+                struct fence_index index = { i, ri };
+                tm_carray_push(fence_indexes, index, a);
+            }
+        }
+        bool *signaled = tm_carray_create(bool, tm_carray_size(fences), a);
+        wait_for_fences(q->backend, fences, signaled, (uint32_t)tm_carray_size(fences));
+
+        for (uint64_t i = 0; i < tm_carray_size(fences); ++i) {
+            if (signaled[i]) {
+                executing_task_t *task = wait_tasks + fence_indexes[i].task_i;
+                // Cannot swap-remove yet, as it will invalidate the indexes.
+                // Just mark the fence so that we can remove it later.
+                task->fences[fence_indexes[i].fence_i].fence_id = 0;
+            }
+        }
+
+        uint64_t task_i = 0;
+        while (task_i < tm_carray_size(wait_tasks)) {
+            executing_task_t *task = wait_tasks + task_i;
+            uint64_t ri = 0;
+            while (ri < tm_carray_size(task->fences)) {
+                if (!task->fences[ri].fence_id)
+                    task->fences[ri] = tm_carray_pop(task->fences);
+                else
+                    ++ri;
+            }
+            if (!tm_carray_size(task->fences)) {
+                tm_carray_free(task->fences, &q->allocator);
+                tm_carray_push(completed_ids, task->task_id, a);
+                wait_tasks[task_i] = tm_carray_pop(wait_tasks);
+            } else {
+                ++task_i;
+            }
+        }
+    }
+
+    return completed_ids;
+}
+
+static inline void free_task_data(task_t *task)
+{
+    if (task->data_allocator)
+        tm_free(task->data_allocator, task->data, task->data_size);
+}
+
+static void process_one_task(mag_async_gpu_queue_o *q)
+{
+    task_t task = { 0 };
+    uint64_t task_id = 0;
+    {
+        TM_OS_ENTER_CRITICAL_SECTION(&q->tasks_cs);
+        if (tm_carray_size(q->task_ids)) {
+            task = heap__pop(q->task_heap, q->task_ids, &task_id);
+        }
+        TM_OS_LEAVE_CRITICAL_SECTION(&q->tasks_cs);
+    }
+
+    if (!task_id)
+        return;
+
+    mag_async_gpu_queue_task_args_t args = {
+        .data = task.data,
+        .fences_allocator = &q->allocator,
+        .task_id = task_id,
+    };
+    task.f(&args);
+    free_task_data(&task);
+    executing_task_t active_task = { .task_id = task_id, .fences = args.out_fences };
+
+    TM_INIT_TEMP_ALLOCATOR_WITH_ADAPTER(ta, a);
+
+    executing_task_t *wait_tasks = NULL;
+    {
+        TM_OS_ENTER_CRITICAL_SECTION(&q->executing_tasks_cs);
+        tm_carray_push(q->executing_tasks, active_task, &q->allocator);
+        if (tm_carray_size(q->executing_tasks) >= q->max_simultaneous_tasks) {
+            wait_tasks = tm_carray_from(q->executing_tasks, tm_carray_size(q->executing_tasks), a);
+            tm_carray_shrink(q->executing_tasks, 0);
+        }
+        TM_OS_LEAVE_CRITICAL_SECTION(&q->executing_tasks_cs);
+    }
+
+    if (wait_tasks) {
+        // Reached simultaneous task limit. Time to block until something completes.
+        uint64_t *completed_ids = wait_for_task(q, wait_tasks, a);
+
+        {
+            TM_OS_ENTER_CRITICAL_SECTION(&q->completed_tasks_cs);
+            for (uint64_t ir = 0; ir < tm_carray_size(completed_ids); ++ir) {
+                uint64_t completed_id = completed_ids[ir];
+                bool is_canceled = false;
+                for (uint64_t ic = 0; ic < tm_carray_size(q->canceled_tasks); ++ic) {
+                    if (q->canceled_tasks[ic] == completed_id) {
+                        is_canceled = true;
+                        q->canceled_tasks[ic] = tm_carray_pop(q->canceled_tasks);
+                        break;
+                    }
+                }
+
+                if (!is_canceled)
+                    tm_carray_push(q->completed_tasks, completed_id, &q->allocator);
+            }
+            TM_OS_LEAVE_CRITICAL_SECTION(&q->completed_tasks_cs);
+        }
+
+        if (tm_carray_size(wait_tasks)) {
+            TM_OS_ENTER_CRITICAL_SECTION(&q->executing_tasks_cs);
+            tm_carray_push_array(q->executing_tasks, wait_tasks, tm_carray_size(wait_tasks), &q->allocator);
+            TM_OS_LEAVE_CRITICAL_SECTION(&q->executing_tasks_cs);
+        }
+    }
+
+    TM_SHUTDOWN_TEMP_ALLOCATOR(ta);
+}
+
+static void wait_for_all_executing_tasks(mag_async_gpu_queue_o *q)
+{
+    TM_INIT_TEMP_ALLOCATOR_WITH_ADAPTER(ta, a);
+    executing_task_t *wait_tasks = NULL;
+
+    TM_OS_ENTER_CRITICAL_SECTION(&q->executing_tasks_cs);
+    if (tm_carray_size(q->executing_tasks)) {
+        wait_tasks = tm_carray_from(q->executing_tasks, tm_carray_size(q->executing_tasks), a);
+        tm_carray_shrink(q->executing_tasks, 0);
+    }
+    TM_OS_LEAVE_CRITICAL_SECTION(&q->executing_tasks_cs);
+
+    while (tm_carray_size(wait_tasks)) {
+        wait_for_task(q, wait_tasks, a);
+    }
+
+    TM_SHUTDOWN_TEMP_ALLOCATOR(ta);
+}
+
+static void thread_entry(void *user_data)
+{
+    mag_async_gpu_queue_o *q = (mag_async_gpu_queue_o *)user_data;
+    while (true) {
+        tm_os_api->thread->semaphore_wait(q->sem);
+        if (q->shutdown) {
+            // need to wait for in-flight tasks so that the fences are not leaked
+            wait_for_all_executing_tasks(q);
+            break;
+        }
+        process_one_task(q);
+    }
+}
+
+static mag_async_gpu_queue_o *create(tm_allocator_i *parent, tm_renderer_backend_i *backend, const mag_async_gpu_queue_params_t *params)
+{
+    tm_os_thread_api *thread_api = tm_os_api->thread;
+
+    tm_allocator_i a = tm_allocator_api->create_child(parent, "mag_async_gpu_queue");
+    mag_async_gpu_queue_o *q;
+    q = tm_alloc(&a, sizeof(*q));
+    *q = (mag_async_gpu_queue_o) {
+        .allocator = a,
+        .backend = backend,
+        .next_id = 1,
+        .sem = thread_api->create_semaphore(0),
+        .max_simultaneous_tasks = params->max_simultaneous_tasks,
+    };
+    thread_api->create_critical_section(&q->tasks_cs);
+    thread_api->create_critical_section(&q->completed_tasks_cs);
+    thread_api->create_critical_section(&q->executing_tasks_cs);
+    q->thread = thread_api->create_thread(thread_entry, q, THREAD_STACK_SIZE, "mag_async_gpu_queue");
+    return q;
+}
+
+static void destroy(mag_async_gpu_queue_o *q)
+{
+    tm_os_thread_api *thread_api = tm_os_api->thread;
+
+    atomic_fetch_add_uint32_t(&q->shutdown, 1);
+    thread_api->semaphore_add(q->sem, 1);
+
+    thread_api->wait_for_thread(q->thread);
+
+    thread_api->destroy_semaphore(q->sem);
+    thread_api->destroy_critical_section(&q->tasks_cs);
+    thread_api->destroy_critical_section(&q->executing_tasks_cs);
+    thread_api->destroy_critical_section(&q->completed_tasks_cs);
+
+    for (uint64_t i = 0; i < tm_carray_size(q->task_heap); ++i) {
+        task_t *task = q->task_heap + i;
+        free_task_data(task);
+    }
+
+    tm_carray_free(q->task_heap, &q->allocator);
+    tm_carray_free(q->task_ids, &q->allocator);
+    tm_carray_free(q->executing_tasks, &q->allocator);
+    tm_carray_free(q->completed_tasks, &q->allocator);
+    tm_carray_free(q->canceled_tasks, &q->allocator);
+
+    tm_allocator_i a = q->allocator;
+    tm_free(&a, q, sizeof(*q));
+    tm_allocator_api->destroy_child(&a);
+}
+
+static uint64_t submit_task(mag_async_gpu_queue_o *q, const mag_async_gpu_queue_task_params_t *params)
+{
+    const uint64_t id = atomic_fetch_add_uint64_t(&q->next_id, 1);
+    const task_t t = {
+        .f = params->f,
+        .data = params->data,
+        .data_size = params->data_size,
+        .data_allocator = params->data_allocator,
+        .priority = params->priority,
+    };
+
+    TM_OS_ENTER_CRITICAL_SECTION(&q->tasks_cs);
+    heap__push(&q->task_heap, &q->task_ids, t, id, &q->allocator);
+    TM_OS_LEAVE_CRITICAL_SECTION(&q->tasks_cs);
+
+    tm_os_api->thread->semaphore_add(q->sem, 1);
+    return id;
+}
+
+static bool update_task_priority(mag_async_gpu_queue_o *q, uint64_t task_id, uint64_t new_priority)
+{
+    bool found = false;
+    TM_OS_ENTER_CRITICAL_SECTION(&q->tasks_cs);
+    for (uint32_t i = 0; i < tm_carray_size(q->task_ids); ++i) {
+        if (q->task_ids[i] == task_id) {
+            found = true;
+            if (new_priority != q->task_heap[i].priority) {
+                q->task_heap[i].priority = new_priority;
+                heap__update(q->task_heap, q->task_ids, i);
+            }
+            break;
+        }
+    }
+    TM_OS_LEAVE_CRITICAL_SECTION(&q->tasks_cs);
+    return found;
+}
+
+static void cancel_task(mag_async_gpu_queue_o *q, uint64_t task_id)
+{
+    bool found = false;
+    {
+        TM_OS_ENTER_CRITICAL_SECTION(&q->tasks_cs);
+        for (uint32_t i = 0; i < tm_carray_size(q->task_ids); ++i) {
+            if (q->task_ids[i] == task_id) {
+                found = true;
+                free_task_data(q->task_heap + i);
+                heap__remove(q->task_heap, q->task_ids, i);
+                break;
+            }
+        }
+        TM_OS_LEAVE_CRITICAL_SECTION(&q->tasks_cs);
+    }
+
+    if (!found) {
+        TM_OS_ENTER_CRITICAL_SECTION(&q->completed_tasks_cs);
+        for (uint64_t i = 0; i < tm_carray_size(q->completed_tasks); ++i) {
+            if (q->completed_tasks[i] == task_id) {
+                q->completed_tasks[i] = tm_carray_pop(q->completed_tasks);
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+            tm_carray_push(q->canceled_tasks, task_id, &q->allocator);
+        TM_OS_LEAVE_CRITICAL_SECTION(&q->completed_tasks_cs);
+    }
+}
+
+static bool is_task_done(mag_async_gpu_queue_o *q, uint64_t task_id)
+{
+    bool completed = false;
+    {
+        TM_OS_ENTER_CRITICAL_SECTION(&q->completed_tasks_cs);
+        for (uint64_t i = 0; i < tm_carray_size(q->completed_tasks); ++i) {
+            if (q->completed_tasks[i] == task_id) {
+                q->completed_tasks[i] = tm_carray_pop(q->completed_tasks);
+                completed = true;
+                break;
+            }
+        }
+        TM_OS_LEAVE_CRITICAL_SECTION(&q->completed_tasks_cs);
+        if (completed)
+            return true;
+    }
+
+    {
+        TM_OS_ENTER_CRITICAL_SECTION(&q->executing_tasks_cs);
+        for (uint64_t i = 0; i < tm_carray_size(q->executing_tasks); ++i) {
+            executing_task_t *task = q->executing_tasks + i;
+            if (q->executing_tasks[i].task_id == task_id) {
+                uint64_t ri = 0;
+                while (ri < tm_carray_size(task->fences)) {
+                    if (q->backend->read_complete(q->backend->inst, task->fences[ri].fence_id, task->fences[ri].device_affinity_mask))
+                        task->fences[ri] = tm_carray_pop(task->fences);
+                    else
+                        ++ri;
+                }
+                if (!tm_carray_size(task->fences)) {
+                    // all fences are done
+                    tm_carray_free(task->fences, &q->allocator);
+                    q->executing_tasks[i] = tm_carray_pop(q->executing_tasks);
+                    completed = true;
+                }
+                break;
+            }
+        }
+        TM_OS_LEAVE_CRITICAL_SECTION(&q->executing_tasks_cs);
+    }
+
+    return completed;
+}
+
+static struct mag_async_gpu_queue_api queue_api = {
+    .create = create,
+    .destroy = destroy,
+    .submit_task = submit_task,
+    .update_task_priority = update_task_priority,
+    .cancel_task = cancel_task,
+    .is_task_done = is_task_done,
+};
+
+TM_DLL_EXPORT void tm_load_plugin(struct tm_api_registry_api *reg, bool load)
+{
+    tm_temp_allocator_api = tm_get_api(reg, tm_temp_allocator_api);
+    tm_allocator_api = tm_get_api(reg, tm_allocator_api);
+    tm_os_api = tm_get_api(reg, tm_os_api);
+
+    tm_set_or_remove_api(reg, load, mag_async_gpu_queue_api, &queue_api);
+}
---
+++ plugins/mag_async_gpu_queue/mag_async_gpu_queue.h
@@ -0,0 +1,66 @@
+#pragma once
+
+struct tm_temp_allocator_i;
+struct tm_allocator_i;
+struct tm_renderer_backend_i;
+
+#include <foundation/api_types.h>
+
+typedef struct mag_async_gpu_queue_o mag_async_gpu_queue_o;
+
+typedef struct mag_async_gpu_queue_fence_t
+{
+    uint32_t fence_id;
+    uint32_t device_affinity_mask;
+} mag_async_gpu_queue_fence_t;
+
+typedef struct mag_async_gpu_queue_task_args_t
+{
+    uint64_t task_id;
+    void *data;
+
+    // carray of fences that denote completion of the task
+    mag_async_gpu_queue_fence_t *out_fences;
+    // the allocator to use when pushing to the out_fences carray
+    struct tm_allocator_i *fences_allocator;
+} mag_async_gpu_queue_task_args_t;
+
+typedef struct mag_async_gpu_queue_params_t
+{
+    uint32_t max_simultaneous_tasks;
+} mag_async_gpu_queue_params_t;
+
+typedef struct mag_async_gpu_queue_task_params_t
+{
+    void (*f)(mag_async_gpu_queue_task_args_t *args);
+    void *data;
+    tm_allocator_i *data_allocator;
+    uint64_t data_size;
+    uint64_t priority;
+} mag_async_gpu_queue_task_params_t;
+
+// Priority queue for submitting GPU tasks. The tasks execute asynchronously with a limit
+// on the number of simultaneously executing tasks.
+// Notice that either cancel_task() or a positive is_task_done() is expected to be called for every
+// submitted task.
+struct mag_async_gpu_queue_api
+{
+    mag_async_gpu_queue_o *(*create)(tm_allocator_i *a, struct tm_renderer_backend_i *backend, const mag_async_gpu_queue_params_t *params);
+
+    // Destroys the queue and the associated resources.
+    // Blocks until the executing tasks are completed to avoid leaking the read fences.
+    void (*destroy)(mag_async_gpu_queue_o *q);
+
+    // Lower priority values are executed first. 0 - top priority.
+    // If the data_allocator is not NULL, it will be used to free the data pointer
+    uint64_t (*submit_task)(mag_async_gpu_queue_o *q, const mag_async_gpu_queue_task_params_t *params);
+
+    // Returns false if the task was not in the queue. This usually means the task is executing/done.
+    bool (*update_task_priority)(mag_async_gpu_queue_o *q, uint64_t task_id, uint64_t new_priority);
+
+    void (*cancel_task)(mag_async_gpu_queue_o *q, uint64_t task_id);
+
+    bool (*is_task_done)(mag_async_gpu_queue_o *q, uint64_t task_id);
+};
+
+#define mag_async_gpu_queue_api_version TM_VERSION(1, 0, 0)
